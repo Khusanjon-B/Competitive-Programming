3af8b40d7d4c45167977a84c584d9bfdb8158987
Khusanjon-B <kbobokhojaev@callutheran.edu>
2026-07-21 16:26:34 -0700

    up

--- update_readme.py
+++ update_readme.py
@@ -76,8 +76,10 @@ Welcome to my competitive programming repository! This repository automatically 
 
 ## Profiles & Stats
 * **Total Solved in Repo:** {total_solved}
-* **Codeforces:** [YourHandle](https://codeforces.com/profile/YourHandle)
-* **CSES:** [YourHandle](https://cses.fi/user/YourID)
+* **Codeforces:** [khusanjonb](https://codeforces.com/profile/khusanjonb)
+* **CSES:** [Khusanjon](https://cses.fi/user/384721)
+* **HackerRank:** [kbobokhojaev](https://www.hackerrank.com/profile/kbobokhojaev)
+* **Kattis:** [kb](https://open.kattis.com/users/khusanjon-bobokhojaev)
 
 ---
 
