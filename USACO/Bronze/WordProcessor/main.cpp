e273ec4e004570989111f27b51026496df087fd2
Khusanjon-B <kbobokhojaev@callutheran.edu>
2026-07-23 16:36:02 -0700

    new

---
+++ USACO/Bronze/WordProcessor/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // Redirect stdin and stdout to USACO files
+    freopen("word.in", "r", stdin);
+    freopen("word.out", "w", stdout);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<string> sentences;
+
+    int currCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        if (sentences.empty()) {
+            sentences.push_back(s);
+            currCount = s.length();
+        } else if (currCount + s.length() <= k) {
+            sentences[sentences.size() - 1] += (" " + s);
+            currCount += s.length();
+        } else {
+            sentences.push_back(s);
+            currCount = s.length();
+        }
+    }
+    for (int i = 0; i < sentences.size(); i++) {
+        cout << sentences[i] << "\n";
+    }
+
+}
